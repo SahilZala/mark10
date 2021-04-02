c2c23fd4ddf47c5d61bd49cbbe1533bde2c660a4
SahilZala <58364647+SahilZala@users.noreply.github.com>
2021-04-02 12:28:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function auth(){
           
             console.log(window.recaptchaVerifier)
 
-             firebase.auth().signInWithPhoneNumber("+918459888289", window.recaptchaVerifier)
+             firebase.auth().signInWithPhoneNumber("+91 8459888289", window.recaptchaVerifier)
             .then((confirmationResult) => {
 
                 console.log("otp send");
